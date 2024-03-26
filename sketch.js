fd539d87fe0da49418bb6d833e0331db56a95cb1
Farkhod Ummatov <118482570+FarhotUmmatov@users.noreply.github.com>
2024-03-26 20:17:58 +0100

    Generate 16x16 grid of square divs in container div

--- sketch.js
+++ sketch.js
@@ -1,4 +1,15 @@
-containerDiv = document.querySelector('#grid');
-squareDivs = document.createElement('div');
-squareDivs.classList.add('square');
-containerDiv.appendChild(squareDivs);
+
+document.addEventListener("DOMContentLoaded", function() {
+    containerDiv = document.querySelector('#container');
+    
+    for (let i = 0; i < 255; i++) {
+        squareDivs = document.createElement('div');
+        squareDivs.classList.add('square');
+        squareDivs.classList.add('cell');
+        containerDiv.appendChild(squareDivs);
+        
+    }
+    
+  });
+
+  
