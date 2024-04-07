fd8994ee8c19188fb8d1e71d99b757a6ac8a61cd
Farkhod Ummatov <118482570+FarhotUmmatov@users.noreply.github.com>
2024-04-07 16:42:48 +0200

    hover effect
    a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen

--- sketch.js
+++ sketch.js
@@ -7,9 +7,23 @@ document.addEventListener("DOMContentLoaded", function() {
         squareDivs.classList.add('square');
         squareDivs.classList.add('cell');
         containerDiv.appendChild(squareDivs);
-        
     }
-    
+    let squares = document.querySelectorAll('.square'); // Alle Quadrat-Divs auswählen
+        
+    squares.forEach(function(square) {
+            // Event-Listener hinzufügen, um die Hintergrundfarbe zu ändern, 
+            // wenn das Quadrat-Div gehovert wird
+        square.addEventListener('mouseover', function() {
+            // Zufällige RGB-Werte generieren
+            let red = Math.floor(Math.random() * 256);
+            let green = Math.floor(Math.random() * 256);
+            let blue = Math.floor(Math.random() * 256);
+                
+            // Neue Hintergrundfarbe festlegen
+            let randomColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+            square.style.backgroundColor = randomColor;
+            });
+    });
   });
 
   
